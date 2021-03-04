a8f2bae5453333eb53a53d12e66832ba44550d83
Edu-Estudios <eduardo.ml.trabajo@gmail.com>
2021-03-04 16:03:21 +0100

    Initial Commit

---
+++ Bullseye SwiftUI/Bullseye_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bullseye_SwiftUIApp.swift
+//  Bullseye SwiftUI
+//
+//  Created by Eduardo Martin Lorenzo on 04/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct Bullseye_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
